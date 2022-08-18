43b8bb60954daf6af1a6c13a9c4dbc3b54052b9f
Arian <arianrdcas@gmail.com>
2022-08-18 00:14:15 -0400

    rutas get y post ok

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db
+from models import db, User, Project
 
 
 
@@ -20,6 +20,79 @@ CORS(app)
 @app.route("/")
 def main():
     return render_template('index.html')
+
+
+"""-------RUTAS USUARIO-------"""
+
+@app.route("/api/users", methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+
+    return jsonify(users), 200
+
+@app.route("/api/users/register", methods=['POST'])
+def register():
+    name = request.json.get('name')
+    password = request.json.get('password')
+    emailuser = request.json.get('emailuser')
+
+    if not name: return jsonify({"msg": "El nombre es requerido"}), 400
+    if not password: return jsonify({"msg": "La contraseña es requerida"}), 400
+    if not emailuser: return jsonify({"msg": "La contraseña es requerida"}), 400
+
+
+    user = User.query.filter_by(name=name).first()
+    if user: return jsonify({"msg": "El usuario ya existe!!!"}),400
+
+    user = User()
+    user.name = name
+    user.password = password
+    user.emailuser = emailuser
+
+    user.save()
+
+    if not user: return jsonify({"msg": "El registro falló!!!"}),400
+
+    return jsonify(data), 200
+
+
+
+
+
+"""--------RUTAS PROYECTOS--------"""
+
+@app.route("/api/proyectos", methods=['GET'])
+def get_proyectos():
+    proyectos = Project.query.all()
+    proyectos = list(map(lambda proyectos: proyectos.serialize(), proyectos))
+
+    return jsonify(proyectos), 200
+
+@app.route("/api/proyectos/register", methods=['POST'])
+def proyecto():
+    nombre = request.json.get('nombre')
+    descripcion = request.json.get('descripcion')
+    autor = request.json.get('autor')
+
+    if not nombre: return jsonify({"msg": "El nombre del proyecto es requerido"}), 400
+    if not descripcion: return jsonify({"msg": "La descripcion es requerida"}), 400
+    if not autor: return jsonify({"msg": "El autor es requerido"}), 400
+
+
+    proyecto = Project.query.filter_by(nombre=nombre).first()
+    if proyecto: return jsonify({"msg": "El proyecto ya existe!!!"}),400
+
+    proyecto = Project()
+    proyecto.nombre = nombre
+    proyecto.descripcion = descripcion
+    proyecto.autor = autor
+
+    proyecto.save()
+
+    if not proyecto: return jsonify({"msg": "El registro falló!!!"}),400
+
+    return jsonify(), 200 
 
 
 if __name__ == '__main__':
--- backend/src/models.py
+++ backend/src/models.py
@@ -1,2 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'usuario'
+    idusuario = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    emailuser = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+    def serialize(self):
+        return {
+            "idusuario": self.idusuario,
+            "name":self.name,
+            "emailuser": self.emailuser
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+         db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+
+class Project(db.Model):
+    __tablename__ = 'proyecto'
+    idproyecto = db.Column(db.Integer, primary_key=True)
+    nombre  = db.Column(db.String(50), nullable=False, unique=True)
+    descripcion  = db.Column(db.String(100))
+    autor = db.Column(db.String(100), nullable=False)
+
+    def serialize(self):
+        return {
+            "idproyecto": self.idproyecto,
+            "nombre":self.nombre,
+            "descripcion": self.descripcion,
+            "autor": self.autor
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+         db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
